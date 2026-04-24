b1217519fd4bedee4a047c263b03b18eee41254a
Jesus Rodriguez Echenique <j.ehu14@hotmail.com>
2026-04-23 21:08:25 -0500

    Add HealthPotion item implementing IItem

---
+++ Models/HealthPotion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAdventureGame.Interfaces;
+
+
+namespace TextAdventureGame.Models
+{
+    public class HealthPotion : IItem
+    {
+
+        public int HealAmount { get; set; }
+
+        public HealthPotion(int amount)
+        {
+            HealAmount = amount;
+        }
+
+        public void Use(Player player)
+        {
+            player.Heal(HealAmount);
+            Console.WriteLine("Te curaste {HealAmount} de vida.");
+        }
+
+    }
+}
