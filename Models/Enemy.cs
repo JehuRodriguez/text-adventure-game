d843273a8994bab9e95fd9a9f12f29636540f796
Jesus Rodriguez Echenique <j.ehu14@hotmail.com>
2026-04-23 21:00:01 -0500

    Add Enemy classs with health and damage

---
+++ Models/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventureGame.Models
+{
+    public class Enemy
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+
+        public bool IsAlive => Health > 0;
+
+        public Enemy(string name, int health, int damage)
+        {
+            Name = name;
+            Health = health;
+            Damage = damage;
+        }
+
+        public void TakeDamage(int dmg)
+        {
+            Health -= dmg;
+        }
+    }
+}
